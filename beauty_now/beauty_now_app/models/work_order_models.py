d9d6d27042c677e22234652e0a0b3269c74ee8b7
Guillermo A. Moran-Arreola <g.a.moran.arreola@gmail.com>
2021-02-19 07:18:37 -0800

    move place ID to line item

--- beauty_now/beauty_now_app/models/work_order_models.py
+++ beauty_now/beauty_now_app/models/work_order_models.py
@@ -8,14 +8,13 @@ from .customer_profile_models import CustomerProfile
 class WorkOrder(models.Model):
     """
     Work order model.
-    
+
     Arguments:
         models {Model} -- Model class.
     """
     request_date = models.CharField(null=True, max_length=10)
     request_time = models.CharField(null=True, max_length=8)
     customer_profile = models.ForeignKey(CustomerProfile, on_delete=models.DO_NOTHING)
-    place_id = models.CharField(max_length=128, null=True)
     notes = models.CharField(max_length=256, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -27,13 +26,14 @@ class WorkOrder(models.Model):
 class LineItem(models.Model):
     """
     Line item model.
-    
+
     Arguments:
         models {Model} -- Model class.
     """
     service = models.ForeignKey(Service, on_delete=models.DO_NOTHING)
     service_date = models.CharField(null=True, max_length=10)
     service_time = models.CharField(null=True, max_length=8)
+    place_id = models.CharField(max_length=128, null=True)
     quantity = models.IntegerField()
     price = models.IntegerField(default=0)
     status = models.CharField(max_length=32, default='initial_request')
